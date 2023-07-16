4a3a9bc3c5ac131935d4e62b6714ac2603f2f5e9
CHNUTT <chokwittaya.nutt@gmail.com>
2023-07-15 20:56:51 -0500

    [6] - using <Button>

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,23 @@
-import React from "react";
-import { Text, StyleSheet } from "react-native";
-
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
-};
+import React from 'react';
+import { Button, View } from 'react-native';
+import { Text, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30,
   },
 });
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text style={styles.text}>App</Text>
+      <Button
+        onPress={() => {console.log("Button pressed")}}
+        Title='Go to Component Demo'
+      />
+    </View>
+  );
+};
 
 export default HomeScreen;
